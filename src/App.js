7126653ef7ec2c8936952f4bee3e2ee2cd4921da
KinKam Zhao <kamzh88@hotmail.com>
2020-02-07 17:42:44 -0500

    score equal to 12 the player gets an alert saying he wins

--- src/App.js
+++ src/App.js
@@ -23,30 +23,29 @@ class App extends Component {
         this.loadImage();
     }
 
-
     imageClick = id => {
         const newState = { ...this.state };
         let score = newState.score;
         let topScore = newState.topScore;
         let clickID = this.state.clickID;
-        console.log(images);
         if (clickID.length === 0) {
             score++
             clickID.push(id);
-            this.setState({ score: score, topScore: topScore, clickID: this.state.clickID });
-            this.loadImage();
+            this.setState({ score: score, topScore: topScore, clickID: this.state.clickID }); 
         } else {
             if (clickID.includes(id)) {
                 alert("You Lose");
                 this.setState({score: 0, topScore: topScore, clickID: []})
-                this.loadImage();
             } else if (!clickID.includes(id)) {
                 score++
                 clickID.push(id);
-                this.setState({ score: score, topScore: topScore, clickID: this.state.clickID });
-                this.loadImage();
+                this.setState({ score: score, topScore: topScore, clickID: this.state.clickID });       
             }
         }
+        if (score === 12) {
+            alert("You Win!")
+        }
+        this.loadImage();
     }
 
     loadImage() {
