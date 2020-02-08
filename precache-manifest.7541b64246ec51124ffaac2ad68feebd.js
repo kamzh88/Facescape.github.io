cd2f9b1f8133dd0b6f8d3d7a66865c47e521e6ca
KinKam Zhao <kamzh88@hotmail.com>
2020-02-07 19:52:48 -0500

    Updates

---
+++ precache-manifest.7541b64246ec51124ffaac2ad68feebd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41eaaace7569a11e83c275def063a1fb",
+    "url": "/Facescape.github.io/index.html"
+  },
+  {
+    "revision": "5ddb0cc81478dae2a463",
+    "url": "/Facescape.github.io/static/js/2.4f81e427.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Facescape.github.io/static/js/2.4f81e427.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9a3bd13862279299dd6",
+    "url": "/Facescape.github.io/static/js/main.daf4d65a.chunk.js"
+  },
+  {
+    "revision": "77ff1981b7b35be3e015",
+    "url": "/Facescape.github.io/static/js/runtime-main.0e79639c.js"
+  }
+]);
